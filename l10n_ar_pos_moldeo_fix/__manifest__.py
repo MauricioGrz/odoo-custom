7a34bdf462711a5de5747b1b9d3028d391a2d86c
MauricioGrz <mauriciogerez31@gmail.com>
2024-07-30 15:25:07 -0300

    agregar directorio

---
+++ l10n_ar_pos_moldeo_fix/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Moldeo ticket fix',
+    'version': '0.1',
+    'author': 'Ing. Gabriela Rivero',
+    'license': 'LGPL-3',
+    'category': 'Point Of Sale',
+    'website': 'www.galup.com.ar',
+    'depends': ['l10n_ar_pos_einvoice_ticket'],
+    'summary': "Módulo de parche para impresión de ticket de FE con la localización de moldeo v13.0",
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
